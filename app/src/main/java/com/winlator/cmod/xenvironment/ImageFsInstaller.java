cd69d123671d125671e7ac0c3185ece1d99a3701
avavo <usuariousesalv@gmail.com>
2026-08-25 15:48:48 -0300

    fix: refresh bundled Proton 9.0 ARM64EC runtime and bump imagefs version to reinstall on update

--- app/src/main/java/com/winlator/cmod/xenvironment/ImageFsInstaller.java
+++ app/src/main/java/com/winlator/cmod/xenvironment/ImageFsInstaller.java
@@ -37,7 +37,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
 
 public abstract class ImageFsInstaller {
-    public static final byte LATEST_VERSION = 24;
+    public static final byte LATEST_VERSION = 25;
 
     private static void resetContainerImgVersions(Context context) {
         ContainerManager manager = new ContainerManager(context);
